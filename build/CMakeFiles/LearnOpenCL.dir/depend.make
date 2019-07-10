615d73d7b27afc791b0697267271c82ab2c25721
ganler <jaway.liu@gmail.com>
2019-07-10 19:57:16 +0800

    Use cpp17 tuple expression to suit bugs made by intel opencl sdk.

---
+++ build/CMakeFiles/LearnOpenCL.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for LearnOpenCL.
+# This may be replaced when dependencies are built.
---
+++ build/CMakeFiles/OpenCL_Tutorial_in_Codes.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenCL_Tutorial_in_Codes.dir/main.cpp.o"
+  "OpenCL_Tutorial_in_Codes.pdb"
+  "OpenCL_Tutorial_in_Codes"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenCL_Tutorial_in_Codes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
